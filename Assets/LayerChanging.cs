d422a6da4c4e2af8c59f78fcd37e3b5e4f74650a
RogPodge <roliu@microsoft.com>
2020-07-29 01:03:24 -0700

    render pipeline experimentation

---
+++ Assets/LayerChanging.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerChanging : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.layer = 0;
+        
+    }
+}
---
+++ Assets/Materials/Stencil/Stencil.shader
@@ -0,0 +1,21 @@
+﻿Shader "Custom/Stencil"
+{
+	Properties{}
+
+		SubShader{
+
+			Tags {
+				"RenderType" = "Opaque"
+			}
+
+			Stencil{
+				Ref 0
+				Comp NotEqual
+				Pass keep
+			}
+
+			Pass {
+				ZWrite Off
+			}
+	}
+}
